195acb8572f503e5cdd232f2b1c7778c5231c01c
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-08-09 23:46:10 +0300

    feat: create data.js with information about birds

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+const birds = [
+  {
+    id: 1,
+    name: "Скворец",
+    latinName: "Sturnus vulgaris",
+    area: "N. America",
+    description:
+      "Небольшая птица длиной 20—25 см, размахом крыльев около 34—42 см и массой 60—90 г. Тело массивное, с короткой шеей. Клюв довольно длинный, острый и слегка изогнут вниз; в сезон размножения жёлтый, в остальное время тёмно-бурый (основание подклювья всегда голубое).",
+    img: "https://live.staticflickr.com/65535/48907448593_b241a6fd2e_h.jpg",
+    audio:
+      "www.xeno-canto.org/sounds/uploaded/XDCPMYMSWN/XC573430-STUVUL2020-06-08-0530-m.mp3",
+  },
+  {
+    id: 2,
+    name: "Фламинго Джеймса",
+    latinName: "Phoenicoparrus jamesi",
+    area: "S. America",
+    description:
+      "С длиной тела 90—92 см и массой около 2 кг, фламинго Джеймса является одним из самых мелких представителей семейства.Оперение окрашено в нежно-розовый цвет, более яркий ближе к голове. Во время сезона размножения на груди фламинго Джеймса выделяются карминовые прожилки.",
+    img: "https://live.staticflickr.com/860/43567093582_d4598a4195_n.jpg",
+    audio: "https://www.xeno-canto.org/sounds/uploaded/KOIEAHGHNX/63/a01.mp3",
+  },
+  {
+    id: 3,
+    name: "Иволга",
+    latinName: "",
+    area: "",
+    description: "",
+    img: "",
+    audio: "",
+  },
+];
