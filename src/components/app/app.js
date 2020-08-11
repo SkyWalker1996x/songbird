a2bbac3672b53e41fa11dabba0e82d649a0dd48c
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-08-11 22:59:44 +0300

    feat: add to App and Header possibility use stages-data and indicate active stage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "components/header";
 import Wrapper from "components/layouts/wrapper";
 import BirdQuestion from "components/bird-question";
-
 import NextLevel from "components/next-level";
 import AnswerBlock from "../answer-block";
 
-const App = () => {
-  return (
-    <Wrapper>
-      <Header />
-      <BirdQuestion />
-      <AnswerBlock />
-      <NextLevel />
-    </Wrapper>
-  );
-};
+class App extends Component {
+  state = {
+    level: 1,
+  };
+  render() {
+    const { level } = this.state;
+    return (
+      <Wrapper>
+        <Header level={level} />
+        <BirdQuestion />
+        <AnswerBlock />
+        <NextLevel />
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,37 +1,29 @@
 import React from "react";
 import "styles/index.scss";
-import logo from "assets/imgs/songbird-logo.svg"
+import logo from "assets/imgs/songbird-logo.svg";
+import { stages } from "data/data";
 
-const Header = () => {
+const Header = ({ level }) => {
+  const stagesList = stages.map((item) => {
+    const { id, stage } = item;
+    const stageStyle = level === id ? "stage__item" : "stage__item active";
+    return (
+      <li className={stageStyle} key={id}>
+        {stage}
+      </li>
+    );
+  });
+
   return (
     <header className="header">
       <div className="top-panel">
-        <div >
-          <img className="logo" src={logo} alt="songbird-logo"/>
+        <div>
+          <img className="logo" src={logo} alt="songbird-logo" />
         </div>
         <div className="score">Score: 100</div>
       </div>
 
-      <div className="stages">
-        <li className="stage__item active">
-          1
-        </li>
-        <li className="stage__item">
-          2
-        </li>
-        <li className="stage__item">
-          3
-        </li>
-        <li className="stage__item">
-          4
-        </li>
-        <li className="stage__item">
-          5
-        </li>
-        <li className="stage__item">
-          6
-        </li>
-      </div>
+      <div className="stages">{stagesList}</div>
     </header>
   );
 };
