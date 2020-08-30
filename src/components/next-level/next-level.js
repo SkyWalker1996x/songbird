92886b5119bb4faadf766b423b6b25e41a4fd7fe
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-08-30 23:46:36 +0300

    fix: change eng to rus lang

--- src/components/next-level/next-level.js
+++ src/components/next-level/next-level.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NextLevel = ({ levelComplete, onNextLevel }) => {
   return levelComplete ? (
-    <button className="btn-up active" onClick={() => onNextLevel()}>Next Level</button>
+    <button className="btn-up active" onClick={() => onNextLevel()}>Следующий уровень</button>
   ) : (
     <button className="btn-up">Следующий уровень</button>
   );
